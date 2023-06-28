2b20ec46de79b23f84272a289f3f239ca92a2597
InuAimar2020 <muhammad.nuh@sirclo.com>
2023-06-28 12:10:19 +0700

    FEAT(Layouting): fixing techstack section

--- src/components/techStackSection.jsx
+++ src/components/techStackSection.jsx
@@ -15,63 +15,90 @@ const techStackSection = () => {
                 <div className="w-full px-4">
                     <div className="flex flex-wrap items-center justify-center">
                         <a
-                            href="#"
+                            href="https://react.dev/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/react.svg" alt="react" />
                         </a>
                         <a
-                            href="#"
+                            href="https://nextjs.org/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/nextjs.svg" alt="nextjs" />
                         </a>
                         <a
-                            href="#"
+                            href="https://business.adobe.com/products/magento/magento-commerce.html"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/magento.svg" alt="magento" />
                         </a>
                         <a
-                            href="#"
+                            href="https://www.figma.com/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/figma.svg" alt="figma" />
                         </a>
                         <a
-                            href="#"
+                            href="https://helpx.adobe.com/support/xd.html"
+                            target="_blank"
+                            rel="noreferrer"
+                            // eslint-disable-next-line max-len
+                            className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
+                        >
+                            <img src="./img/techstack/AdobeXD.svg" alt="figma" />
+                        </a>
+                        <a
+                            href="https://graphql.org/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/GraphQL.svg" alt="GraphQL" />
                         </a>
                         <a
-                            href="#"
+                            href="https://tailwindcss.com/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/tailwindcss.svg" alt="Tailwindcss" />
                         </a>
                         <a
-                            href="#"
+                            href="https://mui.com/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/material-ui.svg" alt="MaterialUI" />
                         </a>
                         <a
-                            href="#"
+                            href="https://vitejs.dev/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
                             <img src="./img/techstack/vite.svg" alt="vite" />
                         </a>
                         <a
-                            href="#"
+                            href="https://git-scm.com/"
+                            target="_blank"
+                            rel="noreferrer"
                             // eslint-disable-next-line max-len
                             className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8"
                         >
@@ -81,7 +108,7 @@ const techStackSection = () => {
                 </div>
             </div>
         </section>
-    );
-};
+    )
+}
 
-export default techStackSection;
+export default techStackSection
